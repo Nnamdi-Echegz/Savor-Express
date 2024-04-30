f37a7f0ba6a439ebedd996c54857766aa9beb480
Nnamdi-Echegz <121641724+Nnamdi-Echegz@users.noreply.github.com>
2024-04-30 01:06:30 -0400

    cart page desktop completed

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
+import { MdDeleteForever } from "react-icons/md";
 
 const Cart = () => {
   const {cartItems,food_list, removeFromCart} = useContext(StoreContext) 
@@ -21,17 +22,51 @@ const Cart = () => {
             if(cartItems[item._id]>0)
             {
               return (
-                <div className="cart-items-title cart-items-item">
-                    <img src={item.image} alt="" />
-                    <p>{item.name}</p>
-                    <p>{item.price}</p>
-                    <p>{cartItems[item._id]}</p>
-                    <p>{item.price*cartItems[item._id]}</p>
-                    <p>x</p>
-                </div>
+                <div><div className="cart-items-title cart-items-item">
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>${item.price}</p>
+                <p>{cartItems[item._id]}</p>
+                <p>${item.price*cartItems[item._id]}</p>
+                <MdDeleteForever onClick={()=>removeFromCart(item._id)} className='cross'/>   {/* <p className='cross'>X</p> */}
+            </div>
+            <hr />
+            </div>
+                
               )
             }
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr/>
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr/>
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>{0}</b>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>Enter promo code for discount</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder='promocode' />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )

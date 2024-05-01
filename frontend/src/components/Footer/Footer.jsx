87ddb0dc77bb61e99502ce32c4fd6d4819e2a304
Nnamdi-Echegz <121641724+Nnamdi-Echegz@users.noreply.github.com>
2024-04-30 21:07:10 -0400

    footer and placeorder component fix

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-          <img src={assets.logo2} alt="" />
+          <img src={assets.logo2} alt="" className="footer-logo"/>
           <p>
             Explore our delicious menu and order your favorite meals for
             delivery or pickup. We're committed to bringing you quality food
